fe39714752ed678b375b0111e81ccd7947c46eba
bsgelley <bsgelley@gmail.com>
2015-06-21 20:03:52 -0400

    first commit - real service for hand coding

---
+++ Flask Site/handcodingserver/app/views.py
@@ -0,0 +1,10 @@
+from flask import render_template, flash, redirect
+from app import app
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    return render_template('index.html')
+                           #user=user,
+                           #posts=posts)
---
+++ Flask Site/handcodingserver/run.py
@@ -0,0 +1,7 @@
+'''
+Created on Apr 8, 2015
+
+@author: Bluma
+'''
+from app import app
+app.run(debug=True)
